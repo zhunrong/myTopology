725fc988a7d1c3f880dc062504be05353eae2c07
chenzr <chenzr@chd.edu.cn>
2019-07-22 15:57:09 +0800

    增加lib打包配置

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@ const process = require('process');
 const baseConfig = require('./webpack.config');
 const devConfig = require('./webpack.dev.config');
 const prodConfig = require('./webpack.prod.config.js');
+const libConfig = require('./webpack.lib.config');
 // merge()与Object.assign()的区别是：前者会合并对象属性上的数组，后者则是直接替换
 const merge = require('webpack-merge');
 
@@ -11,7 +12,7 @@ module.exports = env => {
             return merge({}, baseConfig(env.NODE_ENV), prodConfig);
         case 'development':
             return merge({}, baseConfig(env.NODE_ENV), devConfig);
-        case 'test':
-            return merge({}, baseConfig(env.NODE_ENV));
+        case 'lib':
+            return libConfig('production');
     }
 }
---
+++ build/webpack.lib.config.js
@@ -0,0 +1,134 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+
+const root = path.resolve(__dirname, '../')
+module.exports = NODE_ENV => ({
+  entry: {
+    // polyfill: "babel-polyfill",
+    graphics: path.resolve(__dirname, '../src/graphics/index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, '../lib'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'web',
+  resolve: {
+    // 创建模块别名，在import require时使用别名会更简洁
+    alias: {
+      '@': path.resolve(root, 'src/')
+    },
+    // 当导入文件没有明确扩展名，将依次使用一下扩展名进行推测
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        // 使用import()动态加载代码时，
+        // 需要安装"babel-plugin-syntax-dynamic-import"插件并配置
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: path.resolve(root, 'src')
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        include: path.resolve(root, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: 'global', // 开启css模块化，但是需要使用:local
+              localIdentName: '[name]-[hash:base64:5]'
+              // namedExport:true
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: 'global', // 开启css模块化，但是需要使用:local
+              localIdentName: '[name]-[hash:base64:5]'
+              // namedExport:true
+            }
+          },
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 10240,
+              name: './static/images/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|woff)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './static/fonts/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // 构建优化
+  optimization: {
+    // 代码分离，将重复代码分割提取到一个单独文件中
+    splitChunks: {
+      // chunks: 'all',
+      // cacheGroups: {
+      //     vendor: {
+      //         test: /[\\/]node_modules[\\/]/,
+      //         name: 'vendors',
+      //         chunks: 'all'
+      //     }
+      // }
+    }
+  },
+  // 监听文件更改选项
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  plugins: [
+    new CleanWebpackPlugin([path.resolve(__dirname, '../lib')], {
+      root: root // 设置项目根目录，根目录之外的文件会跳过删除
+    }),
+    // copy文件
+    new CopyWebpackPlugin([{
+      from: path.resolve(root, 'static'),
+      to: path.resolve(root, 'lib/static')
+    }]),
+    new webpack.ProvidePlugin({
+      // 'editormd': 'editor.md/editormd.min.js'
+    })
+  ],
+  externals: {
+  }
+})
